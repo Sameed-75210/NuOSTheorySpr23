699d6dc2b0825ea66eddf9ba2e63ae29a56fec99
saadProgram <88710028+saadProgram@users.noreply.github.com>
2023-05-05 17:02:31 +0500

    Update main.c

--- Project/main.c
+++ Project/main.c
@@ -1,1 +1,180 @@
-adasdd
+#include <limits.h>
+
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+// This version of the function will also try to delete symbolic links,
+// and will skip any non-regular files (such as device files or sockets). Also, 
+// it won't try to delete a directory that is not empty, avoiding the error message you were seeing. 
+// However, please note that this function still won't be able to delete files that
+// are in use by other processes or have special permissions that prevent their deletion.
+
+void delete_directory_recursive(const char *path)
+{
+    DIR *dir;
+    struct dirent *entry;
+    struct stat st;
+
+    dir = opendir(path);
+    if (dir == NULL)
+    {
+        fprintf(stderr, "Error opening directory (%s): %s\n", path, strerror(errno));
+        return;
+    }
+
+    while ((entry = readdir(dir)) != NULL)
+    {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+        {
+            continue;
+        }
+
+        char full_path[PATH_MAX];
+        snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
+
+        if (stat(full_path, &st) == -1)
+        {
+            fprintf(stderr, "Error getting file information (%s): %s\n", full_path, strerror(errno));
+            continue;
+        }
+
+        if (S_ISREG(st.st_mode) || S_ISLNK(st.st_mode))
+        { // Check if it's a regular file or a symbolic link
+            printf("Deleting file: %s\n", full_path);
+            if (unlink(full_path) == -1)
+            {
+                fprintf(stderr, "Error deleting file (%s): %s\n", full_path, strerror(errno));
+            }
+        }
+        else if (S_ISDIR(st.st_mode))
+        {
+            delete_directory_recursive(full_path);
+        }
+        else
+        {
+            fprintf(stderr, "Skipping non-regular file: %s\n", full_path);
+        }
+    }
+
+    closedir(dir);
+
+    // Try to delete the directory. If it fails because it's not empty, print a warning and continue.
+    if (rmdir(path) == -1 && errno != ENOTEMPTY)
+    {
+        fprintf(stderr, "Error deleting directory (%s): %s\n", path, strerror(errno));
+    }
+}
+
+void clean_user_cache_directories()
+{
+    const char *user_cache_dirs[] = {
+        ".cache",
+        ".mozilla",
+        ".config/google-chrome",
+        ".config/google-chrome-beta",
+        ".config/google-chrome-unstable",
+        ".config/chromium",
+        "snap/firefox/common/.cache",
+        "/home/snap/firefox/common/.mozilla",
+        NULL};
+
+    char *user = getenv("USER");
+    char *home = getenv("HOME");
+
+    if (user != NULL && home != NULL)
+    {
+        for (int i = 0; user_cache_dirs[i] != NULL; ++i)
+        {
+            char full_path[PATH_MAX];
+            snprintf(full_path, sizeof(full_path), "%s/%s", home, user_cache_dirs[i]);
+
+            // Check if directory exists
+            if (access(full_path, F_OK) != 0)
+            {
+                // If directory doesn't exist, print a message and continue to the next directory
+                printf("Directory %s does not exist. Skipping...\n", full_path);
+                continue;
+            }
+
+            delete_directory_recursive(full_path);
+        }
+    }
+    else
+    {
+        printf("Cannot determine the user's home directory. Skipping user-specific cache cleanup.\n");
+    }
+}
+
+int main()
+{
+    int pipefd[2];
+    if (pipe(pipefd) == -1)
+    {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid > 0)
+    {                     // Parent process
+        close(pipefd[0]); // Close unused read end
+
+        // Inform child process that the parent is done
+        if (write(pipefd[1], "done", 5) == -1)
+        {
+            perror("write");
+            exit(EXIT_FAILURE);
+        }
+        close(pipefd[1]); // Close write end
+
+        if (wait(NULL) == -1)
+        {
+            perror("wait");
+            exit(EXIT_FAILURE);
+        }
+        printf("Parent process completed.\n");
+    }
+    else
+    {                     // Child process
+        close(pipefd[1]); // Close unused write end
+
+        char buf[10];
+        ssize_t nbytes = read(pipefd[0], buf, sizeof(buf));
+        close(pipefd[0]); // Close read end
+
+        if (nbytes > 0 && strcmp(buf, "done") == 0)
+        {
+            clean_user_cache_directories();
+        }
+        else
+        {
+            fprintf(stderr, "Error reading from pipe in child process.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Cache cleaned successfully by the child process.\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    return 0;
+}
